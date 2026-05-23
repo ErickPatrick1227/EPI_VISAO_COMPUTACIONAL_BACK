4a6e316eb0e9d9511e2d3e5fdbc2911e532b14b5
Erick Patrick <erickckpatrick17@gmail.com>
2026-05-23 10:59:26 -0300

    chore: inicializa projeto Spring Boot com dependências e configuração

---
+++ src/main/java/tech/safevision/SafevisionApplication.java
@@ -0,0 +1,13 @@
+package tech.safevision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafevisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafevisionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/safevision/SafevisionApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.safevision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafevisionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
